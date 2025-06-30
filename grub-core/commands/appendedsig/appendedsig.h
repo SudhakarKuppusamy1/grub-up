84214ed0f8f58e365a8fd17dcdbebcf5dd148457
Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
2025-06-30 22:26:42 +0530

    appended signatures: Parse PKCS#7 signedData
    This code allows us to parse:
    
     - PKCS#7 signedData messages. Only a single signerInfo is supported,
       which is all that the Linux sign-file utility supports creating
       out-of-the-box. Only RSA, SHA-256 and SHA-512 are supported.
       Any certificate embedded in the PKCS#7 message will be ignored.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>
    Signed-off-by: Sudhakar Kuppusamy <sudhakar@linux.ibm.com>
    Reviewed-by: Stefan Berger <stefanb@linux.ibm.com>
    Reviewed-by: Avnish Chouhan <avnish@linux.ibm.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- grub-core/commands/appendedsig/appendedsig.h
+++ grub-core/commands/appendedsig/appendedsig.h
@@ -17,10 +17,46 @@
  *  along with GRUB.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <grub/crypto.h>
 #include <libtasn1.h>
 
 extern asn1_node grub_gnutls_gnutls_asn;
 extern asn1_node grub_gnutls_pkix_asn;
+
+#define MAX_OID_LEN 32
+
+/* A PKCS#7 signedData signerInfo. */
+struct pkcs7_signerInfo
+{
+  const gcry_md_spec_t *hash;
+  gcry_mpi_t sig_mpi;
+};
+
+/*
+ * A PKCS#7 signedData message.
+ * We make no attempt to match intelligently, so we don't save any info about
+ * the signer.
+ */
+struct pkcs7_signedData
+{
+  int signerInfo_count;
+  struct pkcs7_signerInfo *signerInfos;
+};
+
+/*
+ * Parse a PKCS#7 message, which must be a signedData message.
+ * The message must be in 'sigbuf' and of size 'data_size'. The result is
+ * placed in 'msg', which must already be allocated.
+ */
+extern grub_err_t
+parse_pkcs7_signedData (const void *sigbuf, grub_size_t data_size, struct pkcs7_signedData *msg);
+
+/*
+ * Release all the storage associated with the PKCS#7 message.
+ * If the caller dynamically allocated the message, it must free it.
+ */
+extern void
+pkcs7_signedData_release (struct pkcs7_signedData *msg);
 
 /* Do libtasn1 init. */
 extern int
